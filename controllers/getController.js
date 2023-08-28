6689e30f224e11782f27ad5472b4db28e0777b89
Ronan C <126070278+monority@users.noreply.github.com>
2023-08-28 14:26:42 +0200

    Pull de la branche develop

--- controllers/getController.js
+++ controllers/getController.js
@@ -2,13 +2,13 @@
 const User = require('../models/user');
 
 const getUsers = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.status(200).json(users);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Erreur de récupération des utilisateurs" });
-    }
+	try {
+		const users = await User.find();
+		res.status(200).json(users);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Erreur de récupération des utilisateurs" });
+	}
 };
 
 module.exports = {
